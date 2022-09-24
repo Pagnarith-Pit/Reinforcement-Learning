8215b98eee86a37fea47b862a723556ccf4a92ff
Henry Pit <pit@dyn-59-191-214-26.its.monash.edu.au>
2022-09-24 14:37:07 +1000

    Structuring a Monte Carlo Tree Search

--- agents/t_039/MSTAgent.py
+++ agents/t_039/MSTAgent.py
@@ -2,17 +2,25 @@ from template import Agent
 from Reversi.reversi_model import *
 import random
 
-
-
 class myAgent(Agent):
     def __init__(self,_id):
         super().__init__(_id)
         self.gameRule = ReversiGameRule(2)
-
     
+    def selection(self, game_state):
+        
+        pass
+
+    def expansion():
+        pass
+
+    def simulation():
+        pass
+
+    def backPropogation():
+        pass
+        
     def SelectAction(self,actions,game_state):
         #self.gameRule.generateSuccessor(game_state, actions, self.id)
         # Use -p option to print to console
         return random.choice(actions)
-
-
