b50190e247d75aded88384bace08a389632c0869
TuAoXiaoLongXia <69559269+TuAoXiaoLongXia@users.noreply.github.com>
2022-09-29 22:05:51 +1000

    minimaxjoe v3

--- agents/t_039/MiniMaxJoe.py
+++ agents/t_039/MiniMaxJoe.py
@@ -5,8 +5,8 @@ import math
 import random
 import time
 
-INITIAL_DEPTH = 3 
-CUTOFF = 0
+INITIAL_DEPTH = 0
+CUTOFF = 2
 
 # Initial values of alpha, beta 
 MAX, MIN = math.inf, -math.inf
@@ -24,37 +24,52 @@ class myAgent(Agent):
         """
         Returns state from minimax strategy 
         """
+        actions = list(set(actions))
 
         # Initialize minimax-alpha with alpha-beta pruning with values 
-        self.gameRule.agent_colors = game_state.agent_colors
+        #self.gameRule.agent_colors = game_state.agent_colors
+        return self.actions[0]
 
-        return self.AlphaBetaMiniMax(game_state,game_state,actions)
+        return self.AlphaBetaMiniMax(game_state,self.id,actions)
         #return random.choice(actions)
 
-    def NaiveEval(self, game_state):
+    def NaiveEval(self, game_state,agent_id):
         """
         Computes a naive evaluation score of the board based on 
         number of disc current player minus number of disc of the
         opposition"""
-        return self.gameRule.calScore(game_state, self.id) \
-            - self.gameRule.calScore(game_state, (self.id + 1)%2)
+        return self.gameRule.calScore(game_state, agent_id) \
+            - self.gameRule.calScore(game_state, (agent_id + 1)%2)
 
 
 
-    def AlphaBetaMiniMax(self, game_state,agent_id,actions):
+    def AlphaBetaMiniMax(self,game_state,agent_id,actions):
+        actions = list(set(actions))
         depth = INITIAL_DEPTH
         # initialize alpha, beta 
         alpha, beta = MIN, MAX 
+        return self.actions[0]
 
         action_successor_states = [(action,self.gameRule.generateSuccessor(self, game_state, action, agent_id)) for action in actions]
 
+        op_id = self.gameRule.getNextAgentIndex()
+
+        print("degbug")
+
+        return self.actions[0]
+
         for (action, successor_state) in action_successor_states:
             score = self.MinValue(successor_state,alpha,beta,agent_id,depth-1)
+            print("hello")
+            print(f"score: {score}")
             if score > alpha: 
                 alpha = score 
                 self.bestAction = action
+                print(f"best action: {self.bestAction}")
 
+        return self.actions[0]
         return self.bestAction
+        print(self.bestAction)
 
 
     # Implement MiniMax with Alpha-Beta pruning 
@@ -65,29 +80,28 @@ class myAgent(Agent):
 
         # IF CUTOFF-Test (have not checked end game conditions)
         if depth == CUTOFF: 
-            return self.NaiveEval(game_state)
+            return self.NaiveEval(game_state,agent_id)
+            return 1 
 
         # find successors: For each s in successors(state) do
 
-        ## Get legal actions 
-        actions = self.gameRule.getLegalActions(self, game_state, agent_id)
+        ## Get legal actions (of opponent?)
+        actions = self.gameRule.getLegalActions(self,game_state,agent_id)
 
         # check length of actions?
         if len(actions) == 0: 
-            return self.MinValue(game_state,alpha,beta,self.gameRule.getNextAgentIndex(),depth-1)
+            return self.MinValue(game_state,alpha,beta,agent_id,depth-1)
 
         ## apply -> get successor states 
         successor_states = [self.gameRule.generateSuccessor(self, game_state, action, agent_id) for action in actions]
 
-        # loop 
-        # get opponent id 
-        op_id = self.gameRule.getNextAgentIndex()
+
         #for action, successor_state in zip(actions,successor_states):
             #if self.MinValue(self,successor_state,alpha,beta,op_id,depth-1) >= alpha:
                 #self.bestAction = action
                 #alpha = self.MinValue(self,successor_state,alpha,beta,op_id,depth-1) 
         for successor_state in successor_states:
-            alpha = max(alpha, self.MinValue(self,successor_state,alpha,beta,op_id,depth-1))
+            alpha = max(alpha, self.MinValue(self,successor_state,alpha,beta,agent_id,depth-1))
             if alpha >= beta:
                 return beta 
         return alpha 
@@ -99,25 +113,25 @@ class myAgent(Agent):
 
         # IF CUTOFF-Test (have not checked end game conditions)
         if depth == CUTOFF: 
-            return self.NaiveEval(game_state)
+            return self.NaiveEval(game_state,(agent_id + 1)%2)
+            return 1 
 
         # find successors: For each s in successors(state) do
 
         ## Get legal actions 
-        actions = self.gameRule.getLegalActions(self, game_state, agent_id)
+        op_id = self.gameRule.getNextAgentIndex()
+        actions = self.gameRule.getLegalActions(self, game_state, op_id)
 
         # check length of actions?
         if len(actions) == 0: 
-            return self.MaxValue(game_state,alpha,beta,self.gameRule.getNextAgentIndex(),depth-1)
+            return self.MaxValue(game_state,alpha,beta,agent_id,depth-1)
 
-        ## apply -> get successor states 
-        successor_states = [self.gameRule.generateSuccessor(self, game_state, action, agent_id) for action in actions]
+        successor_states = [self.gameRule.generateSuccessor(self, game_state, action, op_id) for action in actions]
 
         # loop 
         # get opponent id 
-        op_id = self.gameRule.getNextAgentIndex()
         for successor_state in successor_states:
-            beta = min(beta, self.MaxValue(self,successor_state,alpha,beta,op_id,depth-1))
+            beta = min(beta, self.MaxValue(self,successor_state,alpha,beta,agent_id,depth-1))
             if beta >= alpha: 
                 return alpha 
         return beta
